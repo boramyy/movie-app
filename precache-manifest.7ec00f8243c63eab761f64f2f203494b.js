13e99634d59ce3416a7d7494eeeb13fd4dee19e1
boramkim <boramyyo@gmail.com>
2019-09-08 14:10:13 +0900

    Updates

---
+++ precache-manifest.7ec00f8243c63eab761f64f2f203494b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9c8a984fcea5a2b218d1b2e61afd163",
+    "url": "/movie-app/index.html"
+  },
+  {
+    "revision": "22cad12fcd5e940eb248",
+    "url": "/movie-app/static/css/main.07bb0469.chunk.css"
+  },
+  {
+    "revision": "d6236eacf8accf481677",
+    "url": "/movie-app/static/js/2.c50870f4.chunk.js"
+  },
+  {
+    "revision": "22cad12fcd5e940eb248",
+    "url": "/movie-app/static/js/main.53ca383a.chunk.js"
+  },
+  {
+    "revision": "79193e64042208c676e3",
+    "url": "/movie-app/static/js/runtime~main.3c14c9f9.js"
+  }
+]);
